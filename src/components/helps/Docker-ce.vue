f0c2f12e3d9218349bfeaf0d98b37e1c96ddd07a
Zhenxin <fuzhenxin95@gmail.com>
2021-04-07 22:17:47 +0800

    Fix bug

--- src/components/helps/Docker-ce.vue
+++ src/components/helps/Docker-ce.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Docker-ce镜像使用指南</h1>
     <p>
-      <customized-markdown :content="docker-ce_content" v-on:render-complete="$emit('render-complete')">
+      <customized-markdown :content="docker_ce_content" v-on:render-complete="$emit('render-complete')">
       </customized-markdown>
     </p>
   </div>
@@ -13,12 +13,12 @@ export default {
   name: 'Docker-ce',
   data () {
     return {
-      docker-ce_content: ''
+      docker_ce_content: ''
     }
   },
   mounted () {
     this.$axios.get('/static/help/Docker-ce.md').then((response) => {
-      this.docker-ce_content = response.data
+      this.docker_ce_content = response.data
     })
   }
 }
