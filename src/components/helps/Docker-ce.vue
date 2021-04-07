d4fde42a11865b1a8f4d13fd136ff942ad47b654
Zhenxin <fuzhenxin95@gmail.com>
2021-04-07 21:53:27 +0800

    Link two help page

---
+++ src/components/helps/Docker-ce.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Docker-ce镜像使用指南</h1>
+    <p>
+      <customized-markdown :content="anaconda_content" v-on:render-complete="$emit('render-complete')">
+      </customized-markdown>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Anaconda',
+  data () {
+    return {
+      anaconda_content: ''
+    }
+  },
+  mounted () {
+    this.$axios.get('/static/help/Docker-ce.md').then((response) => {
+      this.anaconda_content = response.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+p {
+  text-align: left;
+}
+</style>
