22c2edb4f51ceb0c4b8c9eadf42af7b6b89cc54d
ouyen <ouyen@users.noreply.github.com>
2023-02-23 13:15:40 +0800

    add openwrt help

---
+++ src/components/helps/Openwrt.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Openwrt镜像使用指南</h1>
+    <p>
+      <customized-markdown :content="openwrt_content" v-on:render-complete="$emit('render-complete')">
+      </customized-markdown>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Openwrt',
+  data () {
+    return {
+      openwrt_content: ''
+    }
+  },
+  mounted () {
+    this.$axios.get('/static/help/Openwrt.md').then((response) => {
+      this.openwrt_content = response.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+p {
+  text-align: left;
+}
+</style>
